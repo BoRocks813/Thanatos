e7cca241a5ad95f6d5a19a78e4f6dda445dd4eee
BoRocks813 <72171117+BoRocks813@users.noreply.github.com>
2021-04-15 17:26:08 -0500

    Thanatos 6.1

--- Thanatos/src/main/java/frc/robot/auton/autonDrive.java
+++ Thanatos/src/main/java/frc/robot/auton/autonDrive.java
@@ -2,6 +2,7 @@ package frc.robot.auton;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.RobotContainer;
+import frc.robot.subsystems.Chassis;
 import edu.wpi.first.wpilibj.Timer;
 
 public class autonDrive extends CommandBase {
@@ -10,11 +11,13 @@ public class autonDrive extends CommandBase {
     double rotateSpeed;
     double time;
 
+    Chassis chassis;
+
     Timer m_Timer;
 
     boolean isFinished = false;
     
-    public autonDrive(double speed, double rotate, double amtTime) {
+    public autonDrive(double speed, double rotate, double amtTime, Chassis chassis) {
         addRequirements(RobotContainer.m_Chassis);
 
         m_Timer = new Timer();
@@ -22,6 +25,7 @@ public class autonDrive extends CommandBase {
         driveSpeed = speed;
         rotateSpeed = rotate;
         time = amtTime;
+        this.chassis = chassis;
     }
 
     @Override
@@ -31,7 +35,7 @@ public class autonDrive extends CommandBase {
 
     @Override
     public void execute() {
-        RobotContainer.m_Chassis.driveChassis(driveSpeed, rotateSpeed);
+        chassis.driveChassis(driveSpeed, rotateSpeed);
     
 
         if(m_Timer.get() >= time) {
